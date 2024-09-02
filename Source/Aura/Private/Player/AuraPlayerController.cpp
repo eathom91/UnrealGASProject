3ec09767c2a0ec3a947548c4401e4129f0704978
eathom91 <eathom91@gmail.com>
2024-09-02 01:03:53 -0400

    added player controller class

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,30 @@
+// Copyright Private
+
+
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext); // assert aura context is valid
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem); // assert subsystem is valid
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
