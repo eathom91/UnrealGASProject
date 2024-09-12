e6d1ea525fb995f2eac479d11b6c7ed4c8aa5c78
eathom91 <eathom91@gmail.com>
2024-09-11 20:51:09 -0400

    added abilityactorinfo initializers

--- Source/Aura/Private/Player/AuraPlayerState.cpp
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -8,11 +8,14 @@
 
 AAuraPlayerState::AAuraPlayerState()
 {
-	NetUpdateFrequency = 100.f;
+	
 	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
 	AbilitySystemComponent->SetIsReplicated(true);
 	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
 	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+
+	NetUpdateFrequency = 100.f;
 }
 
 UAbilitySystemComponent* AAuraPlayerState::GetAbilitySystemComponent() const
